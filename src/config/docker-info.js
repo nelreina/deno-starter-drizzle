8b4ff35a5bebf9525eb6c7ef7c06ebf90961b6e4
Nelson Reina (@nelreina) <nelreina@gmail.com>
2025-01-18 20:41:00 -0400

    pass container name or service name as consumer name

---
+++ src/config/docker-info.js
@@ -0,0 +1,23 @@
+import { Docker } from "https://deno.land/x/dockerapi@v0.1.1/mod.ts";
+
+// Create a new Docker client
+const docker = new Docker("/var/run/docker.sock");
+const HOSTNAME = Deno.env.get("HOSTNAME") || "localhost";
+export async function getRunningContainerName() {
+  try {
+    // Fetch the list of currently running containers
+    const containers = await docker.containers.list();
+
+    // Iterate through the containers and print their details
+    for (const container of containers) {
+      const info = await container.inspect();
+      if (info.Config.Hostname === HOSTNAME) {
+        return info.Name;
+      }
+    }
+    return null;
+  } catch (error) {
+    console.error("Error fetching containers:", error);
+    return null;
+  }
+}
--- src/config/redis-client.js
+++ src/config/redis-client.js
@@ -1,13 +1,13 @@
 import "https://deno.land/x/logging@v2.0.0/mod.ts";
-import {RedisClient} from "@nelreina/redis-client";
-
-
+import { RedisClient } from "@nelreina/redis-client";
+import { getRunningContainerName } from "./docker-info.js";
+const SERVICE_NAME = Deno.env.get("SERVICE_NAME");
 export const client = new RedisClient({
   redisHost: Deno.env.get("REDIS_HOST"),
   redisPort: Deno.env.get("REDIS_PORT"),
   redisUser: Deno.env.get("REDIS_USER"),
   redisPw: Deno.env.get("REDIS_PW"),
-  serviceName: Deno.env.get("SERVICE_NAME"),
-  timeZone : Deno.env.get("TIME_ZONE"),
-
+  serviceName: SERVICE_NAME,
+  timeZone: Deno.env.get("TIMEZONE"),
+  streamConsumerName: await getRunningContainerName() || SERVICE_NAME.toLocaleLowerCase(),
 });
